a99f101510892e78b3ef5ab030ca4bad5bd92404
photop33 <75907575+photop33@users.noreply.github.com>
2021-03-21 18:23:07 +0200

    Update MainScore.py

--- MainScore.py
+++ MainScore.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
 import Utils
 from Score import get
 app = Flask(__name__)
+from MainGame import MainGame
+from  Live import load_game
 
 @app.route('/')
 def score_server():
@@ -32,6 +33,19 @@ def score_server():
         </body>
     </html>"""
 
+@app.route('/gamepicker')
+def game_picker():
+    res=MainGame()
+
+@app.route('/guessgame')
+def gussgame():
+    load_game(2)
+    print ("finish guessgame")
+
+@app.route('/memorygame')
+def memorygame():
+    load_game(1)
+    print ("finish memorygame")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True, threaded=True, port=8777)
