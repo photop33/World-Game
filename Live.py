8b7bb1e6c30f830bbed0a65f18d7dd7236d3253f
photop33 <75907575+photop33@users.noreply.github.com>
2021-03-21 18:24:04 +0200

    Update Live.py

--- Live.py
+++ Live.py
@@ -8,33 +8,27 @@ def welcome(name):
 
 
 
-def load_game():
+def load_game(x):
     print('Please choose a game to play:')
     print('1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back')
     print('2. Guess Game - guess a number and see if you chose like the computer')
-    c= int(input('choose number "1" or "2" '))
+    # c= int(input('choose number "1" or "2" '))
     print('-----------------------------------------------------------------------')
     difficulty=int(input('Please choose game difficulty from 1 to 5'))
-    if c == 1:
-        print('Lets play Memory Game')
-        from MemoryGame import play
-        play(difficulty)
+    if x == 1:
+        def Memory_Game():
+            print('Lets play Memory Game')
+            from MemoryGame import play
+            play(difficulty)
+        Memory_Game()
 
-    elif c == 2:
-        print('Lets play Guess Game')
-        from GuessGame import play
-        play(difficulty)
+    elif x == 2:
+        def Guess_Game():
+            print('Lets play Guess Game')
+            from GuessGame import play
+            play(difficulty)
+        Guess_Game()
 
     else:
         print("Something went wrong..")
     print('-----------------------------------------------------------------------')
-
-
-
-
-
-
-
-
-
-
